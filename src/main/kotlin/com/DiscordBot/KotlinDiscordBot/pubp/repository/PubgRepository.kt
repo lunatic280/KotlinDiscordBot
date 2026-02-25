887c8682c1a718a3d001e633ca0755adfda85df4
lunatic280 <rick0201@nave.com>
2026-02-25 23:26:35 +0900

    배그 아이디 입력 기능

---
+++ src/main/kotlin/com/DiscordBot/KotlinDiscordBot/pubp/repository/PubgRepository.kt
@@ -0,0 +1,12 @@
+package com.DiscordBot.KotlinDiscordBot.pubp.repository
+
+import com.DiscordBot.KotlinDiscordBot.pubp.domain.PubgPlayers
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PubgRepository : JpaRepository<PubgPlayers, Long> {
+
+    fun findByMember_UserId(userId: String): PubgPlayers
+    fun existByPlayerId(playerId: String): Boolean
+}
